c28684da75c94d12ed5c876af5e0ed193d01b0fe
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-06-04 17:07:39 -0300

    modularização py

---
+++ ex109.py
@@ -0,0 +1,7 @@
+import uteis
+
+n = int(input('DIGITE UM NÚMERO ->'))
+fat = uteis.fatorial(n)
+print(f' FATORIAL DE {n} É {fat}')
+print(f'O DOBRO DO {n} É IGUAL {uteis.fatorial(n)}')
+print(f'O TRIPLO DO {n} É IGUAL {uteis.fatorial(n)}')
