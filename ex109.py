d0654de35e9ca3005cc1f2ee20d89849c6363255
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-06-05 17:48:00 -0300

    aprendendo modularização e pacotes

--- ex109.py
+++ ex109.py
@@ -1,7 +1,10 @@
-import uteis
+from uteis import numeros
 
-n = int(input('DIGITE UM NÚMERO ->'))
-fat = uteis.fatorial(n)
-print(f' FATORIAL DE {n} É {fat}')
-print(f'O DOBRO DO {n} É IGUAL {uteis.fatorial(n)}')
-print(f'O TRIPLO DO {n} É IGUAL {uteis.fatorial(n)}')
+n = int(input('DIGITE UM NÚMERO PARA SER MANIPULADO->'))
+p = int(input('DIGITE UM NÚMERO INTEIRO QUE VAI SER USADO COMO PORCENTAGEM ->'))
+print(f'='*60)
+print(f'O DOBRO DO NÚMERO {n} É {numeros.dobro(n)}')
+print(f'A METADE DO NÚMERO {n} É {numeros.metade(n)}')
+print(f'VAMOS AUMENTAR O SEU NÚMERO DE ACORDO COM A PORCENTAGEM COLOCADA\n VAI SER AUMENTADO O NÚMERO {n} E O TOTAL PÓS PORCENTAGEM É {numeros.aumentar(n,p)}')
+print(f'='*60)
+print(f'VAMOS DIMINUIR O SEU NÚMERO DE ACORDO COM A PORCENTAGEM COLOCADA\n VAI SER DIMINUIDO O NÚMERO {n} E O TOTAL PÓS PORCENTAGEM É {numeros.diminuir(n,p)}')
