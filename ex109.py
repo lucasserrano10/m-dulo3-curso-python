23f5e1440eb4a0cac80a41adb4ccd78d36ff88d5
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-06-06 12:16:31 -0300

    exercícios módulos

--- ex109.py
+++ ex109.py
@@ -1,10 +1,10 @@
 from uteis import numeros
 
-n = int(input('DIGITE UM NÚMERO PARA SER MANIPULADO->'))
-p = int(input('DIGITE UM NÚMERO INTEIRO QUE VAI SER USADO COMO PORCENTAGEM ->'))
+n = int(input('DIGITE O PREÇ0 -> R$'))
+p = int(input('DIGITE UM NÚMERO INTEIRO PARA PORCENTAGEM->'))
 print(f'='*60)
-print(f'O DOBRO DO NÚMERO {n} É {numeros.dobro(n)}')
-print(f'A METADE DO NÚMERO {n} É {numeros.metade(n)}')
-print(f'VAMOS AUMENTAR O SEU NÚMERO DE ACORDO COM A PORCENTAGEM COLOCADA\n VAI SER AUMENTADO O NÚMERO {n} E O TOTAL PÓS PORCENTAGEM É {numeros.aumentar(n,p)}')
+print(f'O DOBRO DO NÚMERO {numeros.moeda(n)} É {numeros.moeda(numeros.dobro(n))}')
+print(f'A METADE DO NÚMERO {numeros.moeda(n)} É {numeros.moeda(numeros.metade(n))}')
+print(f'AUMENTANDO {p}% temos {numeros.moeda(numeros.aumentar(n,p))}')
+print(f'DIMINUINDO {p}% temos {numeros.moeda(numeros.diminuir(n,p))}')
 print(f'='*60)
-print(f'VAMOS DIMINUIR O SEU NÚMERO DE ACORDO COM A PORCENTAGEM COLOCADA\n VAI SER DIMINUIDO O NÚMERO {n} E O TOTAL PÓS PORCENTAGEM É {numeros.diminuir(n,p)}')
